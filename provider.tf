99bcedae97b4523845f7b464e53ec410020edacb
naresh <Naresh@gmail.com>
2024-06-21 11:37:53 +0530

    mini

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+ }
+ 
+    }
+backend "s3" {
+    bucket = "eternal-s3-dev"
+    key    = "minikube"
+    region = "us-east-1"
+    dynamodb_table = "s3-table-dev"
+    }
+
+}
